b160d60935ddb00bfa7c4cf0d424bfd7593dc8b5
Reshuv <reshu0899@gmail.com>
2022-05-05 23:28:12 +0530

    Learning datatypes

--- 03_variable/02_datatype.go
+++ 03_variable/02_datatype.go
@@ -2,8 +2,21 @@ package main
 
 import "fmt"
 
+//Declared variable with identifier z is of type string
+var y = 42
+var z = "strange"
+
+//back ticks
+
+var a string = `James said, "Shaken, not stirred`
+
 func main() {
-	var y = "Reshu"
+
 	fmt.Println(y)
 	fmt.Printf("%T", y)
+	fmt.Println(z)
+	fmt.Printf("%T", z)
+	fmt.Println(a)
+	fmt.Printf("%T", a)
+
 }
