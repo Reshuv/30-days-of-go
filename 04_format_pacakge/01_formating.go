987c1443e850d4eff1752637e1ed53650824598c
Reshuv <reshu0899@gmail.com>
2022-05-06 00:03:43 +0530

    format Package

---
+++ 04_format_pacakge/01_formating.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+var y = 42
+
+func main() {
+	fmt.Println(y)
+	fmt.Printf("%T\n", y)
+	fmt.Printf("%b\n", y)
+	fmt.Printf("%x\n", y)
+	y = 822
+	fmt.Printf("%#x\n", y)
+	fmt.Printf("%#x\t%b\t%x\t%T\n", y, y, y, y)
+
+	s := fmt.Sprintf("%#x\t%b\t%x\t%T\t", y, y, y, y)
+	fmt.Println(s)
+	fmt.Printf("%v", y)
+
+}
