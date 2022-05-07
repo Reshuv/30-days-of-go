05a9e1aafe8b9a650a718207f0ac78675133c839
Reshuv <reshu0899@gmail.com>
2022-05-07 23:59:07 +0530

    Exercise

---
+++ 05_hands_on_excise.go/01_Exceise1.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 42
+	y := "James Bond"
+	z := true
+	fmt.Println(x, y, z) //single print statement
+	fmt.Println(x)       //multiple print statement
+	fmt.Println(y)
+	fmt.Println(z)
+}
