df7d4a7b07189d88a2226e2e2fcafd5a2234de2a
Reshuv <reshu0899@gmail.com>
2022-05-12 02:17:53 +0530

    String typr

--- 06_Datatypes/05_string_type.go
+++ 06_Datatypes/05_string_type.go
@@ -3,12 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	x := "Hello World"
-	fmt.Println(x)
-	fmt.Printf("%T\n", x)
+	s := "Hello World"
+	fmt.Println(s)
+	fmt.Printf("%T\n", s)
 
-	bs := []byte(x)
+	bs := []byte(s)
 	fmt.Println(bs)
-	fmt.Printf("%T", bs)
+	fmt.Printf("%T\n", bs)
+
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("%#U\n", s[i])
+	}
+	fmt.Println("")
+
+	for i, v := range s {
+		fmt.Printf("at index position %d we have hex %#x\n", i, v)
+	}
 
 }
