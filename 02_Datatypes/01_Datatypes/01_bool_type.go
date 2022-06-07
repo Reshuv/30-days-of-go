1167f1ac922411e39285d9d03885eacaedcf11d1
Reshuv <reshu0899@gmail.com>
2022-06-08 05:29:19 +0530

    datatypes

--- 02_Datatypes/01_Datatypes/01_bool_type.go
+++ 02_Datatypes/01_Datatypes/01_bool_type.go
@@ -7,11 +7,11 @@ var x bool
 func main() {
 	fmt.Println(x) //false because we dont have assign value to x so by default go assign value "0"
 	x = true
-	fmt.Println(x)//assigned a value so true
+	fmt.Println(x) //assigned a value so true
 
 	a := 21
 	b := 45
-	c := 21
+	c := 2
 	fmt.Println(a == b) //comparision operator
 	fmt.Println(c == c)
 	fmt.Println(b > c)
