3a7bb7399a827593d5e5b1661385b4bf830459c7
Reshuv <reshu0899@gmail.com>
2022-06-09 20:40:28 +0530

    arrays

---
+++ 04_grouping_gata/01_arrays.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a [5]int // array is collection of similar type of elements
+	var b [6]int // array type is different because of it's len
+
+	a[3] = 42
+	fmt.Println(a)
+	fmt.Println(len(a)) // len of an array
+	fmt.Println(b)
+
+}
